2a7e79d0eeeb799cd9c7f3e50c1bc8154330b74f
stvno <github@minst.net>
2011-06-15 15:30:56 +0200

    ported mqLayerControl to new API, still need to prettify it

---
+++ src/jquery.mapquery.mqLayerControl.js
@@ -0,0 +1,75 @@
+(function($) {
+$.template('mqLayerControl',
+    '<li class="mq-layercontrol ui-widget ui-helper-clearfix ">'+
+    '<span class="label"><div class="ui-icon ui-icon-arrowthick-2-n-s"></div>${label} (${position})</span>' +
+    '<button>Delete</button>' +
+    '<input type="checkbox" class="mq-layercontrol-visibility" id="${id}-visibility" checked="checked" />'+
+    '<label for="${id}-visibility">Visible</label>'+
+    '</li>');
+        
+$.widget("mapQuery.mqLayerControl", {
+    _create: function() {
+        var map;
+        var self = this;
+        var element = this.element;
+        
+        if (this.options.jquery === $().jquery) {
+            map = this.options.data('mapQuery');
+            this.options = {};
+        }
+        else {
+            map = this.options.map.data('mapQuery');
+        }
+        element.append('<ul></ul>');
+        var ulElement = element.children('ul');
+        $.each(map.layers().reverse(), function() {
+            self._add(ulElement, this);
+        });
+        
+        element.find('button').button();
+        ulElement.sortable({
+            update: function(event, ui) {
+                var layerNodes = ui.item.siblings().andSelf();
+                var num = layerNodes.length-1;
+                layerNodes.each(function(i) {
+                    var layer = $(this).data('layer');
+                    var pos = num-i;
+                    var label = $('span.label', this);
+                    var icon = label.children();
+                    layer.position(pos);
+                    label.text(layer.label + ' (' + pos + ')')
+                        .prepend(icon);
+                });
+            }
+        });
+
+        // bind events for the layer controls
+        element.delegate('.mq-layercontrol-visibility', 'change', function() {
+            var checkbox = $(this);
+            var layer = checkbox.parents('li').data('layer');
+            checkbox.attr('checked') ? layer.visible(true) : layer.visible(false);
+        });
+
+        element.delegate('button', 'click', function() {
+            var control = $(this).parents('li');
+            control.data('layer').remove();
+            control.fadeOut(function() {
+                $(this).remove();
+            });
+        });
+    },
+    _add: function(element, layer) {
+        //$.tmpl('mqLayerControl', layer)
+        // We don't need to pass in the whole layer object
+        $.tmpl('mqLayerControl', {
+            id: layer.id,
+            label: layer.label,
+            position: layer.position()
+        })
+            // save layer layer ID in the DOM, so we can easily
+            // hide/show/delete the layer with live events
+            .data('layer', layer)
+            .prependTo(element);
+    }
+});
+})(jQuery);
