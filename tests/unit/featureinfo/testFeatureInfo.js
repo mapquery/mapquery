6206e708b00a4d8d8597cee03b7813203d7bd1e3
Volker Mische <volker.mische@gmail.com>
2011-06-23 17:06:38 +0200

    Test for previous feature info widgets improvements.
    And some cleanups.

--- tests/unit/featureinfo/testFeatureInfo.js
+++ tests/unit/featureinfo/testFeatureInfo.js
@@ -25,12 +25,14 @@ asyncTest("Feature Info is shown", 3, function() {
         var feature = data.object.features[0];
         layer.events.triggerEvent('featureselected', {feature: feature});
         start();
-        equals($('#info1').find('p').length, 1, 'Popup contains data');
-        equals($('#info1').find('p').text(), 'poly1-1', 'Feature 1 was selected');
+        equals($('#info1').find('p').length, 1, 'Feature Info contains data');
+        equals($('#info1').find('p').text(), 'poly1-1',
+               'Feature 1 was selected');
 
         feature = data.object.features[1];
         layer.events.triggerEvent('featureselected', {feature: feature});
-        equals($('#info1').find('p').text(), 'poly1-2', 'Feature 2 was selected');
+        equals($('#info1').find('p').text(), 'poly1-2',
+               'Feature 2 was selected');
 
         mq.destroy();
         info.empty();
@@ -38,7 +40,7 @@ asyncTest("Feature Info is shown", 3, function() {
 });
 
 asyncTest("Feature Info bound to two nodes", 5, function() {
-    var map = $('#map1').mapQuery({
+    var map = $('#map2').mapQuery({
         layers: {
             type: 'JSON',
             label: 'Polygons',
@@ -60,7 +62,7 @@ asyncTest("Feature Info bound to two nodes", 5, function() {
         var feature = data.object.features[0];
         layer.events.triggerEvent('featureselected', {feature: feature});
         start();
-        equals($('#info2').find('p').length, 1, 'Popup contains data');
+        equals($('#info2').find('p').length, 1, 'Feature Info contains data');
         equals($('#info2').find('p').text(), 'poly1-1',
                'Feature 1 was selected (#info2)');
         equals($('#info3').find('p').text(), 'poly1-1',
@@ -72,6 +74,38 @@ asyncTest("Feature Info bound to two nodes", 5, function() {
                'Feature 2 was selected (#info2)');
         equals($('#info3').find('p').text(), 'poly1-2',
                'Feature 2 was selected (#info3)');
+
+        mq.destroy();
+        info.empty();
+    });
+});
+
+asyncTest("Feature Info is hidden feature gets unselected", 3, function() {
+    var map = $('#map3').mapQuery({
+        layers: {
+            type: 'JSON',
+            label: 'Polygons',
+            url: '../../../demo/data/poly.json'
+        }
+    });
+    var info = $('#info4').mqFeatureInfo({
+        map: map,
+        contents: function(feature) {
+            return '<p>' + feature.data.id + '</p>';
+        }
+    });
+
+    var mq = map.data('mapQuery');
+    mq.layers()[0].bind('loadend', function(evt, data) {
+        var layer = data.object;
+        var feature = data.object.features[0];
+        layer.events.triggerEvent('featureselected', {feature: feature});
+        start();
+        equals($('#info4').find('p').length, 1, 'Feature Info contains data');
+        equals($('#info4').find('p').text(), 'poly1-1', 'Feature 1 was selected');
+
+        layer.events.triggerEvent('featureunselected', {feature: feature});
+        equals($('#info4').children().length, 0, 'Feature Info is empty');
 
         mq.destroy();
         info.empty();
