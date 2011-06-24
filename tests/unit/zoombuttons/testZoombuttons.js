2e69135de3eaed2a15d24d1caab359c0c6edc447
Steven Ottens <github@minst.net>
2011-06-24 10:21:45 +0200

    Created a test for zoombuttons

---
+++ tests/unit/zoombuttons/testZoombuttons.js
@@ -0,0 +1,40 @@
+/*
+ * Popup unit tests
+ */
+(function($) {
+module('zoombuttons');
+
+test("ZoomButtons are shown", 2, function() {
+    var map = $('#map1').mapQuery({
+       layers:[{       
+            type:'osm' 
+            }]
+    });
+    var mq = map.data('mapQuery');
+    var zoombuttons = $('#zoombutton').mqZoomButtons($('#map1'));
+	equals($('#zoombutton').find('.mq-zoombuttons-plus').length, 1, 'zoombuttons contains plus icon');
+	equals($('#zoombutton').find('.mq-zoombuttons-minus').length, 1, 'zoombuttons contains minus icon');
+	
+});
+
+test("ZoomButtons work", 2, function() {
+    var map = $('#map1').mapQuery({
+       layers:[{       
+            type:'osm' 
+            }]
+    });
+    var mq = map.data('mapQuery');
+	var goto = mq.goto();
+	$(".mq-zoombuttons-plus").trigger('click');
+	goto = mq.goto();
+    equals(goto.zoom, 2,
+             'Zoomed in');
+	$(".mq-zoombuttons-minus").trigger('click');
+	goto = mq.goto();
+    equals(goto.zoom, 1,
+             'Zoomed out');
+	
+	
+});
+
+})(jQuery);
