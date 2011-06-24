1d6b9d19d1127e26b5f9cc8411e9a12e5fd80097
stvno <github@minst.net>
2011-06-24 11:29:07 +0200

    Cleaned up

--- tests/unit/zoombuttons/testZoombuttons.js
+++ tests/unit/zoombuttons/testZoombuttons.js
@@ -6,35 +6,31 @@ module('zoombuttons');
 
 test("ZoomButtons are shown", 2, function() {
     var map = $('#map1').mapQuery({
-       layers:[{       
+        layers:[{
             type:'osm' 
-            }]
+        }]
     });
     var mq = map.data('mapQuery');
     var zoombuttons = $('#zoombutton').mqZoomButtons($('#map1'));
-	equals($('#zoombutton').find('.mq-zoombuttons-plus').length, 1, 'zoombuttons contains plus icon');
-	equals($('#zoombutton').find('.mq-zoombuttons-minus').length, 1, 'zoombuttons contains minus icon');
-	
+    equals($('#zoombutton').find('.mq-zoombuttons-plus').length, 1, 'zoombuttons contains plus icon');
+    equals($('#zoombutton').find('.mq-zoombuttons-minus').length, 1, 'zoombuttons contains minus icon');
+
 });
 
 test("ZoomButtons work", 2, function() {
     var map = $('#map1').mapQuery({
-       layers:[{       
+        layers:[{
             type:'osm' 
-            }]
+        }]
     });
     var mq = map.data('mapQuery');
-	var goto = mq.goto();
-	$(".mq-zoombuttons-plus").trigger('click');
+    var goto = mq.goto();
+    $(".mq-zoombuttons-plus").trigger('click');
+    goto = mq.goto();
+    equals(goto.zoom, 2, 'Zoomed in');
+    $(".mq-zoombuttons-minus").trigger('click');
 	goto = mq.goto();
-    equals(goto.zoom, 2,
-             'Zoomed in');
-	$(".mq-zoombuttons-minus").trigger('click');
-	goto = mq.goto();
-    equals(goto.zoom, 1,
-             'Zoomed out');
-	
-	
+    equals(goto.zoom, 1, 'Zoomed out');
 });
 
 })(jQuery);
