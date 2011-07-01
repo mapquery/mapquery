f1559ca5b4d7d58af2fd0ad5d4ae826cffbbd671
stvno <github@minst.net>
2011-07-01 21:04:26 +0800

    Basic tests for all widgets plus TODOs for extra tests

---
+++ tests/unit/mouseposition/testMousePosition.js
@@ -0,0 +1,23 @@
+/*
+ * Overviewmap unit tests
+ */
+(function($) {
+module('mouseposition');
+
+test("Mouseposition is shown", 1, function() {
+    var map = $('#map1').mapQuery({
+        layers:[{
+            type:'osm' 
+        }]
+    });
+    var mq = map.data('mapQuery');
+    var mouseposition = $('#mouseposition').mqMousePosition({map:'#map1'});
+    equals($('#mouseposition').find('#mq-mouseposition-x').length, 1, 'mouseposition contains a x-position field');
+    
+    mq.destroy();
+    mouseposition.empty();
+});
+
+//TODO: add tests for moving the mouse
+
+})(jQuery);
