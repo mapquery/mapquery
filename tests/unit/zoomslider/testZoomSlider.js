cbbb12c721e4ecaf42761c7bbb5b4497c9890d3f
stvno <github@minst.net>
2011-07-01 14:32:42 +0200

    Added a zoomslider test

---
+++ tests/unit/zoomslider/testZoomSlider.js
@@ -0,0 +1,34 @@
+/*
+ * Popup unit tests
+ */
+(function($) {
+module('zoomslider');
+
+test("ZoomSlider is shown", 1, function() {
+    var map = $('#map1').mapQuery({
+        layers:[{
+            type:'osm' 
+        }]
+    });
+    var mq = map.data('mapQuery');
+    var zoombuttons = $('#zoomslider').mqZoomSlider({map:'#map1'});
+    equals($('#zoomslider').find('.ui-slider ').length, 1, 'zoomslider contains slider');
+
+});
+
+test("ZoomSlider works", 2, function() {
+    var map = $('#map1').mapQuery({
+        layers:[{
+            type:'osm' 
+        }]
+    });
+    var mq = map.data('mapQuery');
+    var goto = mq.goto();
+    $(".mq-zoomslider-slider").slider('value',19-4);
+    goto = mq.goto();
+    equals(goto.zoom, 4, 'Zoomed in to 4 using slider');
+    mq.goto({zoom:3});
+    equals($(".mq-zoomslider-slider").slider('value'), 19-3, 'Zoomed out to 3, slider is 3');
+});
+
+})(jQuery);
